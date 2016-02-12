97b85fb4dc83cb6bf7fb4fe5223faf865aabbd3e
Edvaldo Mantovanelli <Edvaldo Mantovanelli>
2016-02-12 13:06:51 -0200

    Primeira versao

---
+++ webservice/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Comunidade
+from .models import Tag
+from .models import Localizacao
+from .models import Usuario
+from .models import PontoTuristico
+from .models import EstabelecimentoFixo
+from .models import Datas
+
+
+# Register your models here.
+admin.site.register(Comunidade)
+admin.site.register(Tag)
+admin.site.register(Localizacao)
+admin.site.register(Usuario)
+admin.site.register(PontoTuristico)
+admin.site.register(EstabelecimentoFixo)
+admin.site.register(Datas)
---
+++ webservice/models.py
@@ -0,0 +1,112 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Comunidade(models.Model):
+    nomeComunidade = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nomeComunidade
+
+class Tag(models.Model):
+    nomeTag = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nomeTag
+
+class Elemento(models.Model):
+    comunidadeElemento = models.ForeignKey(Comunidade)
+    nome = models.CharField(max_length=100)
+    enderecoOficial = models.CharField(max_length=500)
+    enderecoUsual = models.CharField(max_length=500)
+    descricao = models.TextField()
+    listaTags = models.ManyToManyField(Tag)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
+
+class Localizacao(models.Model):
+    pontoElemento = models.ForeignKey(Elemento, on_delete=models.CASCADE)
+
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return str(self.pontoElemento)
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=80)
+    email = models.EmailField(max_length=150)
+    def __str__(self):
+        return self.nome
+
+
+class Comentario(models.Model):
+    elementoComentario = models.ForeignKey(Elemento)
+    nomeUsuario = models.ForeignKey(Usuario)
+    textoComentario = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nomeUsuario
+
+
+class PontoTuristico(Elemento):
+    pass
+
+class Estabelecimento(Elemento):
+    nomeProprietario = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+class Datas(models.Model):
+    SEGUNDA_FEIRA = 'SEG'
+    TERÇA_FEIRA = 'TER'
+    QUARTA_FEIRA = 'QUA'
+    QUINTA_FEIRA = 'QUI'
+    SEXTA_FEIRA = 'SEX'
+    SABADO = 'SAB'
+    DOMINGO = 'DOM'
+
+    DIAS_DA_SEMANA = (
+    (SEGUNDA_FEIRA, 'Segunda-Feira'),
+    (TERÇA_FEIRA, 'Terça-Feira'),
+    (QUARTA_FEIRA, 'Quarta-Feira'),
+    (QUINTA_FEIRA, 'Quinta-Feira'),
+    (SEXTA_FEIRA, 'Sexta-Feira'),
+    (SABADO, 'Sábado'),
+    (DOMINGO, 'Domingo')
+    )
+
+    dia = models.CharField(max_length = 3, choices=DIAS_DA_SEMANA)
+    horaInicio = models.TimeField(auto_now=False, auto_now_add=False)
+    horaFim = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.dia
+
+class EstabelecimentoFixo(Estabelecimento):
+    listaDatas = models.ManyToManyField(Datas)
+
+class EstabelecimentoMovel(Estabelecimento):
+    pass
+
+
+class Agenda(models.Model):
+    agendaMovel = models.ForeignKey(EstabelecimentoMovel)
+    listaDatas = models.ManyToManyField(Datas)
+
+
+
+class Evento(Elemento, models.Model):
+
+    dataEvento = models.DateField(auto_now=False, auto_now_add=False)
+    horaInicio = models.TimeField(auto_now=False, auto_now_add=False)
+    horaFim = models.TimeField(auto_now=False, auto_now_add=False)
+
+class Obra(models.Model):
+    titulo = models.CharField(max_length=80)
+
+class Artista(Elemento, models.Model):
+    listaObras = models.ManyToManyField(Obra)
